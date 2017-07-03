2fa6117efb79b02f812182a6f2268c7daf52a140
aniruddha <aniruddha.kudalkar@gmail.com>
2017-07-03 15:31:01 +0530

    brushup : typescript , es6 intro

---
+++ day2/basics.ts
@@ -0,0 +1,12 @@
+var empSal: number = 1000;
+
+function getSalTs(): number {
+    return empSal;
+}
+
+//empSal = 'xyz';
+
+var empNm = 'pqr';
+
+//empNm = 1234;
+console.log('Name - ' + empNm + ' Sal - ' + getSalTs());
---
+++ day2/new.js
@@ -0,0 +1,40 @@
+var empSal = 1000;
+
+function getSal() {
+    return '';
+}
+
+empSal = 'xyz';
+
+//ES6 
+
+let st = 10;
+if (true) {
+    let st = 20;
+}
+console.log(st);
+
+let tax = function (num1, num2) {
+    return num1 * num2;
+}
+
+function params( num1, salCalc ){
+    salCalc();
+}
+
+function funRet() {
+    return function() {
+        return function() {
+            return 'xyz';
+        };
+    }
+}
+
+tax(10, 23);
+
+params(123, function() {});
+
+let ft = funRet();
+let ft1 = ft();
+console.log('Ft - '+ft1());
+
